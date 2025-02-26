edbcc1497b7aed83750a3eaea44d975bdfe69656
aghabidareh <aghabidareh@gmail.com>
2025-02-26 05:12:25 -0800

    predictor

---
+++ SpamDetector/detector/modules/loader.py
@@ -0,0 +1,12 @@
+from tensorflow.keras.models import load_model
+from joblib import load
+import json
+
+model = load_model('detector/modules/model.h5')
+
+with open('detector/modules/tokenizer.h5', 'rb') as f:
+    tokenizer = load(f)
+
+with open('detector/modules/params.json' , 'r') as f:
+    params = json.load(f)
+    max_len = params['max_len']
---
+++ SpamDetector/detector/modules/predictor.py
@@ -0,0 +1,11 @@
+from keras_preprocessing.sequence import pad_sequences
+
+from detector.modules.loader import *
+
+def predictor(text):
+    sequence = tokenizer.texts_to_sequences([text])
+    padded_sequences = pad_sequences(sequence , maxlen=max_len)
+
+    prediction = model.predict(padded_sequences)
+
+    return "Spam" if prediction > 0.5 else "Safe"
