370969d438b006823bf38d6e3f0564467edbfda3
aghabidareh <aghabidareh@gmail.com>
2025-02-26 05:18:53 -0800

    showing-the-result

--- SpamDetector/detector/views.py
+++ SpamDetector/detector/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+from detector.modules.predictor import predictor
+
+
 def index(request):
+    if request.method == "POST":
+        text = request.POST.get("text")
+        result = predictor(text)
+        context = {'result' : result}
+        return render(request, 'index.html' , context)
     return render(request, 'index.html')
