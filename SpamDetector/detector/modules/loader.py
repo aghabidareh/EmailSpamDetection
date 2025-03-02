db09f9d795a190d917a07e64f9f89d2aa65e88ef
aghabidareh <aghabidareh@gmail.com>
2025-03-02 05:56:29 -0800

    cleaning

--- SpamDetector/detector/modules/loader.py
+++ SpamDetector/detector/modules/loader.py
@@ -7,6 +7,6 @@ model = load_model('detector/modules/model.h5')
 with open('detector/modules/tokenizer.h5', 'rb') as f:
     tokenizer = load(f)
 
-with open('detector/modules/params.json' , 'r') as f:
+with open('detector/modules/params.json', 'r') as f:
     params = json.load(f)
     max_len = params['max_len']
--- SpamDetector/detector/modules/predictor.py
+++ SpamDetector/detector/modules/predictor.py
@@ -2,9 +2,10 @@ from keras_preprocessing.sequence import pad_sequences
 
 from detector.modules.loader import *
 
+
 def predictor(text):
     sequence = tokenizer.texts_to_sequences([text])
-    padded_sequences = pad_sequences(sequence , maxlen=max_len)
+    padded_sequences = pad_sequences(sequence, maxlen=max_len)
 
     prediction = model.predict(padded_sequences)
 
